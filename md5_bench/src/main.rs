e01d132506c227e4b749e824d90c092fa0969d63
Undiscovered-Data <chaserbrummel@gmail.com>
2022-03-04 23:06:17 -0600

    Add files via upload

---
+++ md5_bench/src/main.rs
@@ -0,0 +1,32 @@
+use md5::{Md5, Digest};
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::time::Instant;
+
+
+fn file_to_vec(the_string: &str) -> Vec<String> {
+    let mut str_vec = Vec::new();
+    let the_file = File::open(the_string).expect("File error");
+    let the_reader = BufReader::new(the_file);
+
+    for line in the_reader.lines() {
+        let trim_line = line.expect("Line error");
+        str_vec.push(trim_line);
+    }
+    str_vec
+}
+
+
+fn main() {
+    let vec_o_strings = file_to_vec("../passed-basic-check.txt");
+    let now1 = Instant::now();
+    for a_string1 in &vec_o_strings {
+        let mut hash1 = Md5::new();
+        hash1.update(a_string1);
+        hash1.finalize();
+        //let res1 = hash1.finalize();
+        //println!("{:x}\n", res1);
+    }
+    let later1 = now1.elapsed().subsec_nanos();
+    println!("{}", later1);
+}
